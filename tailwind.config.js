5b426f6a040b83c36e158f0c7c0293889595ff1e
Fatkur Rizky <muhammadfatkur676@gmail.com>
2024-08-15 12:19:53 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      letterSpacing:{
+        "space":".5em",
+      
+      }
+    },
+  },
+  plugins: [],
+}
+
